f1360910e56a6ab19b8ce08bcb399f73aaf3512f
rougho <rohi@rgho.de>
2023-10-30 11:40:53 +0100

    nucleotides counter

--- Rosalind_problems/necleotides_counter.py
+++ Rosalind_problems/necleotides_counter.py
@@ -3,11 +3,12 @@ import collections
 
 def count_nucleotide_frequency(sequance):
     try:
-        # temp_seq_dict = { "A" : 0, "C" : 0, "G" : 0, "T" : 0,}
-        # for nucleotide in sequance:
-        #     temp_seq_dict[nucleotide] += 1
-        # return temp_seq_dict
         return dict(collections.Counter(sequance))
     except:
         if DNAToolkit.seq_validation(sequance) is False:
            return "Wrong Sequance"
+        
+def count_nucleotides(sequance):
+    counter = count_nucleotide_frequency(sequance)
+    for key in counter:
+        print(f"{key} : {counter[key]}")
--- main.py
+++ main.py
@@ -4,6 +4,4 @@ from dna_toolkits import DNAToolkit as toolkit
 from Rosalind_problems import necleotides_counter as nc
 
 my_seq = toolkit.seq_generator(50)
-counter = nc.count_nucleotide_frequency(my_seq)
-for key in counter:
-    print(f"{key} : {counter[key]}")
+nc.count_nucleotides(my_seq)
