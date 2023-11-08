abb4a7d6da828034b3a3072bcf98ab0185ab74ac
rougho <rohi@rgho.de>
2023-11-08 11:16:51 +0100

    reading frame

--- dna_toolkits/DNAToolkit.py
+++ dna_toolkits/DNAToolkit.py
@@ -4,23 +4,29 @@ import collections
 import utilities as utl
 
 # generate sequance
+
+
 def seq_generator(len):
     return ''.join([random.choice(nucleotides)
                     for nucleotide
                     in range(len)])
+
 
 def count_nucleotide_frequency(sequence):
     try:
         return dict(collections.Counter(sequence))
     except:
         if seq_validation(sequence) is False:
-           return "Wrong Sequance"
-        
+            return "Wrong Sequance"
+
+
 def print_nucleotides_frequency(sequence_dict):
     for key in sequence_dict:
         print(f"{key} : {sequence_dict[key]}")
 
-#check the sequence that its a DNA string
+# check the sequence that its a DNA string
+
+
 def seq_validation(dna_sequance):
     '''Validate a sequence of DNA'''
     temp_nucleotide = dna_sequance.upper()
@@ -29,24 +35,31 @@ def seq_validation(dna_sequance):
             return False
     return temp_nucleotide
 
-#DNA Transcription
+# DNA Transcription
+
+
 def transcription(sequence):
     '''Transcripting a sequence of DNA to RNA'''
     return sequence.replace("T", "U")
 
 # DNA Raplication
+
+
 def reverse_complement_sequence(sequence):
     '''Reverse the complemented sequence'''
     return complement_sequence(sequence)[::-1]
+
 
 def complement_sequence(sequence):
     '''Complement a sequence of DNA'''
     mapping = str.maketrans('ATCG', 'TAGC')
     return sequence.translate(mapping)
 
+
 def gc_content(sequence):
     '''percentage of nitrogenous bases in a DNA/RNA molecule that are either Guanine or Cytosine'''
     return (sequence.count('C') + sequence.count('G')) / len(sequence) * 100
+
 
 def subsequence_gc_content(sequence, section=20):
     result = {}
@@ -59,22 +72,26 @@ def subsequence_gc_content(sequence, section=20):
         }
     return result
 
+
 def compute_max_gc_content(file_path):
     max_gc_dict = {}
     FASTA_dict = utl.FASTA_to_dict(file_path)
-    calculate_values_gcc = {key: gc_content(value) for (key, value) in FASTA_dict.items()}
+    calculate_values_gcc = {key: gc_content(
+        value) for (key, value) in FASTA_dict.items()}
     max_gc = max(calculate_values_gcc, key=calculate_values_gcc.get)
     max_gc_full = {
-        'label' : max_gc[1:],
-        'gc_content' : calculate_values_gcc[max_gc],
-        'sequence' : FASTA_dict[max_gc]
-        }
+        'label': max_gc[1:],
+        'gc_content': calculate_values_gcc[max_gc],
+        'sequence': FASTA_dict[max_gc]
+    }
     return max_gc_full
 
-def sequence_translator(sequence, initialize_position = 0):
+
+def sequence_translator(sequence, initialize_position=0):
     return [DNA_Codons[sequence[position:position + 3]]
-                        for position in range(initialize_position,
-                                              len(sequence) - 2, 3)]
+            for position in range(initialize_position,
+                                  len(sequence) - 2, 3)]
+
 
 def codon_frequency(sequence, aminoacid):
     temp_list = []
@@ -84,6 +101,41 @@ def codon_frequency(sequence, aminoacid):
     frequency_dictionary = dict(collections.Counter(temp_list))
     total_wight = sum(frequency_dictionary.values())
     for seq in frequency_dictionary:
-        frequency_dictionary[seq] = round(frequency_dictionary[seq] 
+        frequency_dictionary[seq] = round(frequency_dictionary[seq]
                                           / total_wight, 2)
     return frequency_dictionary
+
+
+def open_reading_frames(sequence):
+    '''Generate the six reading frames of a DNA sequence'''
+    frames = []
+    frames.append(sequence_translator(sequence, 0))
+    frames.append(sequence_translator(sequence, 1))
+    frames.append(sequence_translator(sequence, 2))
+    frames.append(sequence_translator(
+        reverse_complement_sequence(sequence), 0))
+    frames.append(sequence_translator(
+        reverse_complement_sequence(sequence), 1))
+    frames.append(sequence_translator(
+        reverse_complement_sequence(sequence), 2))
+    return frames
+
+
+def proteins_from_reading_frame(aminoacid_sequnce):
+    '''Compute all possibale proteins in an aminoacid sequence and return list of possible proteins'''
+    current_protein = []
+    proteins = []
+    for aminoacid in aminoacid_sequnce:
+        # stop if stop_sign was found (_)
+        if aminoacid == "_":
+            if current_protein:
+                for p in current_protein:
+                    proteins.append(p)
+                current_protein.clear()
+        else:
+            # start if M found
+            if aminoacid == "M":
+                current_protein.append("")
+            for i in range(len(current_protein)):
+                current_protein[i] += aminoacid
+    return proteins
--- utilities.py
+++ utilities.py
@@ -1,13 +1,21 @@
 import dna_toolkits.DNAToolkit as toolkit
 
+RED = '\033[91m'
+RESET = '\033[0m'
+LIGHTGREEN = '\033[92m'
+LIGHTBLUE = '\033[94m'
+LIGHTYELLOW = '\033[93m'
+LIGHTRED = '\033[91m'
+
+
 def colored(seq):
     bcolors = {
-        'A': '\033[92m',
-        'C': '\033[94m',
-        'G': '\033[93m',
-        'T': '\033[91m',
-        'U': '\033[91m',
-        'reset': '\033[0;0m'
+        'A': LIGHTGREEN,
+        'C': LIGHTBLUE,
+        'G': LIGHTYELLOW,
+        'T': LIGHTRED,
+        'U': RED,
+        'reset': RESET
     }
 
     tmpStr = ""
@@ -20,28 +28,37 @@ def colored(seq):
 
     return tmpStr + '\033[0;0m'
 
+
+def light_blue(text):
+    return LIGHTBLUE + text + RESET
+
+
 def print_subseq_gc_content(subseq_gc_content_dict):
     formatted_str = ""
     for subseq, info in subseq_gc_content_dict.items():
         formatted_str += f"\t\t\tSubsequence: {subseq} | Position: {info['position']:<2} | GC Content: {info['gc_content']:.2f}%\n"
     return formatted_str.strip()
 
+
 def read_file(path):
     '''Reading a file and return lines as a list'''
     with open(path, "r") as file:
         return [l.strip() for l in file.readlines()]
+
 
 def FASTA_validator(FASTA_dict):
     '''Validate the sequence in a dict'''
     try:
         for key, value in FASTA_dict.items():
             if not toolkit.seq_validation(value):
-                print(f"Invalid DNA sequence found:\nLabel: {key}\nSequence: {value}")
+                print(
+                    f"Invalid DNA sequence found:\nLabel: {key}\nSequence: {value}")
                 return False
         return True
     except Exception as e:
         print("An error occurred during FASTA validation:", e)
         return False
+
 
 def FASTA_to_dict(path):
     '''Read FASTA formatted file and validate the sequence and return a dictionary'''
@@ -55,9 +72,16 @@ def FASTA_to_dict(path):
             FASTA_label = line
             FASTA_dict[FASTA_label] = ""
         else:
-            FASTA_dict[FASTA_label] += line.replace(" ","")
+            FASTA_dict[FASTA_label] += line.replace(" ", "")
 
     if FASTA_validator(FASTA_dict):
         return FASTA_dict
     else:
         return None
+
+
+def print_orfs(orfs_list):
+    orfs_string = ""
+    for i in range(len(orfs_list)):
+        orfs_string += orfs_list[i]
+    return orfs_string
